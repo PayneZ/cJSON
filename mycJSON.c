f4892c35169af42c840619fb5ecd4d0920cfedca
PayneZ <201421130016@mail.mail.bnu.edu.cn>
2019-12-16 16:36:48 +0800

    completed number parsing

--- mycJSON.c
+++ mycJSON.c
@@ -100,7 +100,18 @@ static const char *parse_number(cJSON *item, const char *num){
     return num;
 }
 
-static int pow2gt(int x);
+static int pow2gt(int x) {
+    /*
+    given x, find the smallest y of the form 2^n, such that y >= x;
+    */
+    --x;       //x=1000 1111;
+    x |= x>>1; //x>>1 = 0100 0111; x=1100 1111;
+    x |= x>>2; //x>>2 = 0011 0011; x=1111 1111;
+    x |= x>>4; //x>>4 = 0000 1000; x=1110 1111
+    x |= x>>8;
+    x |= x>>16;
+    return x+1;
+}
 
 typedef struct {
     char* buffer;
@@ -108,12 +119,76 @@ typedef struct {
     int offset;
 } printbuffer;
 
-static char* ensure(printbuffer *p, int needed);
+static char* ensure(printbuffer *p, int needed)
+{
+    if(!p || !p->buffer) return 0;
+    needed += p->offset;
+    if(needed <= p->length) return p->buffer + p->offset;
 
-static int update(printbuffer *p);
+    int newsize = pow2gt(needed);
+    char* newbuffer = (char *)cJSON_malloc(newsize);
+    if(!newbuffer) {
+        cJSON_free(p->buffer);
+        p->length = 0, p->offset = 0;
+        return 0;
+    }
+    //else
+    memcpy(newbuffer, p->buffer, p->length);
+    cJSON_free(p->buffer);
+    p->buffer = newbuffer;
+    p->length = newsize;
+    return p->buffer + p->offset;
+}
 
-static char *print_number(cJSON *item, printbuffer *p);
+static int update(printbuffer *p) {
+    char *str;
+    if(!p || !p->buffer) return 0;
+    str = p->buffer + p->offset;
+    return p->offset + strlen(str);
+}
 
-static unsigned parse_hex4(const char *str);
+static char *print_number(cJSON *item, printbuffer *p) 
+{
+    char *str = 0;
+    double d=item->valuedouble;
+    if(d == 0) 
+    {
+        if(p) str=ensure(p, 2);
+        else str(char*)cJSON_malloc(2);
+        if(str) strcpy(str, "0");
+    } 
+    else if (fabs(((double)item->valueint)-d)<= DBL_EPSILON && d<=INT_MAX && d>=INT_MIN) 
+    {
+        if(p) str = ensure(p, 21);
+        else str=(char*)cJSON_malloc(21); /* 2^64 + 1 can be represented in 21 chars. */
+        if(str) sprintf(str, "%d", item->valueint);
+    } 
+    else 
+    {
+        if(p) str = ensure(p, 64);
+        else str = (char*)cJSON_malloc(64);
+        if(str) 
+        {
+            if(fabs(floor(d)-d)<=DBL_EPSILON && fabs(d)<1.0e60) sprinf(str, "%.0f", d);
+            else if(fabs(d)<1.0e-6 || fabs(d)>1.0e9) sprintf(str, "%e", d);
+            else sprintf(str, "%f", d);
+        }
+    }
+    return str;
+}
 
+static unsigned parse_hex4(const char *str)
+{
+    unsigned h=0;
+    if(*str>='0' && *str<='9') h+=(*str)-'0'; else if (*str>='A' && str<='F') h+=10+(*str)-'A'; else if (*str>='a' && *str<='f') h+=10+(*str)-'a'; else return 0;
+    h=h<<4;str++
+    if(*str>='0' && *str<='9') h+=(*str)-'0'; else if (*str>='A' && str<='F') h+=10+(*str)-'A'; else if (*str>='a' && *str<='f') h+=10+(*str)-'a'; else return 0;
+    h=h<<4;str++
+    if(*str>='0' && *str<='9') h+=(*str)-'0'; else if (*str>='A' && str<='F') h+=10+(*str)-'A'; else if (*str>='a' && *str<='f') h+=10+(*str)-'a'; else return 0;
+    h=h<<4;str++
+    if(*str>='0' && *str<='9') h+=(*str)-'0'; else if (*str>='A' && str<='F') h+=10+(*str)-'A'; else if (*str>='a' && *str<='f') h+=10+(*str)-'a'; else return 0;
+    return h;
+}
+
+static const unsigned char firstByteMark[7] = {0x00, 0x00, 0xC0, 0xE0, 0xF0, 0xF8, 0xFC};
 static const char *parse_string(cJSON *item, const char *str);
